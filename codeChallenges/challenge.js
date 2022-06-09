8b890fc13573f6207dfc0a7b3d0de5369fd06896
Colin Ciervo <colinciervo@@gmail.com>
2022-06-09 09:28:13 -0400

    Mild challenge

---
+++ codeChallenges/challenge.js
@@ -0,0 +1,11 @@
+//Write a JavaScript program that defines a variable that stores an Number.
+//The program should print out the String "even" if the Number is even, and the String "odd" if the Number is odd.
+
+var number = 3
+
+if (number % 2 == 0) {
+  console.log("Even");
+}
+else {
+  console.log("Odd")
+}
